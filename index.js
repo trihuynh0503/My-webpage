3844b4bafe17f55255b969d137951b9be4d165fa
mThuynh53 <139397885+mThuynh53@users.noreply.github.com>
2023-10-02 19:00:00 -0400

    update the main page

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const express = require('express')
 
 const app = express()
 const PORT = 4000
-
+const HTTP_PORT = process.env.PORT || 8080; // assign a port
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+app.use(express.static('public'));
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 })
 
 // Export the Express API
