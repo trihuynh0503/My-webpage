d73413d5a26b39234e26ebf05669bae372e49496
mThuynh53 <139397885+mThuynh53@users.noreply.github.com>
2023-10-02 18:44:37 -0400

    implemented web framework Express into my webpage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 This is the code of my webpage
 
+Update my webpage to a web server using Express.js
+
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'); // "require" the Express module
+const path = require('path');
+const app = express(); // obtain the "app" object
+const HTTP_PORT = process.env.PORT || 8080; // assign a port
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+})
+
+
+// start the server on the port and output a confirmation ot the console
+app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
