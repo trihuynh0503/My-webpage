259c6c99b05783422bba7d93a5e57d470e438826
mThuynh53 <139397885+mThuynh53@users.noreply.github.com>
2023-10-02 20:24:33 -0400

    Please run on vercel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 4000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
