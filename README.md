8da7594520d39a68f320411eb1d42827c23bf8f3
mThuynh53 <139397885+mThuynh53@users.noreply.github.com>
2023-10-04 00:18:09 -0400

    Add a navbar to the webpage

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 This is the code of my webpage
 
+I decided to just use vanilla js to see how far I can push myself with the basic first
 Update my webpage to a web server using Express.js
+
+Some videos that help me with the effects so far:
+    - For the sticky navbar: https://www.youtube.com/watch?v=6HFpw5fcaD8
+    - For the scrolling animation effect: https://www.youtube.com/watch?v=T33NN_pPeNI
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,16 @@
 //will handle the scroll animations for web page
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting) entry.target.classList.add('show');
-        else entry.target.classList.remove('show');
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else { entry.target.classList.remove('show');
+        }
     })
+})
+
+window.addEventListener("scroll", ()=>{
+    var navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("sticky_navbar", window.scrollY > 0);
 })
 
 
