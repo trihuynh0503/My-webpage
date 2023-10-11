1a2b1f02f0109fddbb3fb0db5edb70d7c1c0cb51
mThuynh53 <139397885+mThuynh53@users.noreply.github.com>
2023-10-11 19:19:53 -0400

    Add parallax effect for widescreen background

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Update my webpage to a web server using Express.js
 Some videos that help me with the effects so far:
     - For the sticky navbar: https://www.youtube.com/watch?v=6HFpw5fcaD8
     - For the scrolling animation effect: https://www.youtube.com/watch?v=T33NN_pPeNI
-
+    - Using parallax for my header background: https://www.youtube.com/watch?v=1wfeqDyMUx4&t=161s
+    
 Contact Me form Email message will be handled by formSpree
 
--- public/js/app.js
+++ public/js/app.js
@@ -20,6 +20,35 @@ toggleButton.addEventListener('click', ()=>{
     navLinks.classList.toggle('active');
 })
 
+//handle the parallax effect
+let light1 = document.getElementById('light-1');
+let light2 = document.getElementById('light-2');
+let lighthouse = document.getElementById('lighthouse');
+let island = document.getElementById('island');
+let sky = document.getElementById('sky');
+let sun = document.getElementById('sun');
+let cloud = document.getElementById('cloud');
+let sea = document.getElementById('sea');
+let birds = document.getElementById('birds');
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    light1.style.top = value*1.2 + 'px';
+    light2.style.top = value*1.2 + 'px';
+    light2.style.rotate = value*.05 + 'deg';
+    lighthouse.style.top = value*0.1 + 'px';
+    lighthouse.style.left = value*0.1 + 'px';
+
+    // island.style.top = value*.5 + 'px';
+    sky.style.top = value*.7 + 'px';
+    birds.style.left = value + 'px';
+    sun.style.top = value*.5 + 'px';
+    cloud.style.top = value*.1 + 'px';
+    cloud.style.left = -value*.5 + 'px';
+
+    sea.style.top = value*.15 + 'px';
+
+
+})
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
